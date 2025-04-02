56c0bf9541bad6f0f088fa1014d091431719d559
CeciGonzalez123 <cecigm2011@gmail.com>
2025-04-02 16:18:02 -0400

    Subiendo corpus_sources.csv a la carpeta corpus

--- methods/external/quelingua_pipeline-main/run.sh
+++
@@ -1,1 +0,0 @@
-cat ../../pyplexity/clean_mc4-gl.txt |./quelingua_lines > clean_mc4-gl_quelingua.txt
--- methods/external/quelingua_pipeline-main/x.sh
+++
@@ -1,1 +0,0 @@
-cat ../../pyplexity/clean_cc-100-gl.txt |./quelingua_lines > clean_cc-100-gl_quelingua.txt
